47692be18d2e1056ee6b8e07f810e66222483d42
Rajeshwarikota <kotarajeswari003.com>
2023-03-16 21:59:45 +0530

    [Rajeshwari] Adding Equality checking

--- ProgrammingConstructs/Program.cs
+++ ProgrammingConstructs/Program.cs
@@ -11,6 +11,19 @@ namespace ProgrammingConstructs
         static void Main(string[] args)
         {
             Console.WriteLine("....Programming Constructs....");
+
+            Console.WriteLine("Enter the First Integer");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second Integer");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("Both the Integers are equal");
+            }
+            else 
+            {
+                Console.WriteLine("Both the Integers are not equal");
+            }
             Console.ReadLine();
         }
     }
