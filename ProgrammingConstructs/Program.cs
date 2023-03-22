24a42baac34a2bcf6b59cfe7b3387088f2b9f6e9
Rajeshwarikota <kotarajeswari003.com>
2023-03-22 23:51:06 +0530

    [Rajeshwari] Adding Reverse number

--- ProgrammingConstructs/Program.cs
+++ ProgrammingConstructs/Program.cs
@@ -12,7 +12,7 @@ namespace ProgrammingConstructs
         {
             Console.WriteLine("....Programming Constructs....");
 
-            Console.WriteLine("\n1:EqualityCheck\n2:EvenOrNot\n3:CatingEligibility\n4:LargestThreeNumbers\n5:AdmissionEntry\n6:WeekDays\n7:ArithmeticOperation\n8:PowerOfTwo\n9:SumOfSquares\n10:Factorial\n11:FlipCoin\n12:WordReverse\n13:SumOfFirstFive\n14:CountDuplicate\n15:finfing max or min\n16:Reverse triangle pattern\n22:Exit");
+            Console.WriteLine("\n1:EqualityCheck\n2:EvenOrNot\n3:CatingEligibility\n4:LargestThreeNumbers\n5:AdmissionEntry\n6:WeekDays\n7:ArithmeticOperation\n8:PowerOfTwo\n9:SumOfSquares\n10:Factorial\n11:FlipCoin\n12:WordReverse\n13:SumOfFirstFive\n14:CountDuplicate\n15:finfing max or min\n16:Reverse triangle pattern\n17:ReversingNumber\n22:Exit");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -82,6 +82,9 @@ namespace ProgrammingConstructs
                     break;
                 case 16:
                     ReverseTrianglePattern.Reversetriangle();
+                    break;
+                case 17:
+                    ReversingNumber.ReverseNumber();
                     break;
                 default:
                     Console.WriteLine("Enter the correct option");
---
+++ ProgrammingConstructs/ReversingNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructs
+{
+    class ReversingNumber
+    {
+        public static void ReverseNumber()
+        {
+            int num, remainder, reverse = 0;
+            Console.WriteLine("Enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num > 0)
+            {
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num /= 10;
+            }
+            Console.WriteLine("Reversed number is" + reverse);
+
+        }
+    }
+}
